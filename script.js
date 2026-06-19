8e7b85b7632f356096fc38f0ce56cf8476793d39
sdoylegrady <sdoylegrady@hotmail.com>
2026-06-19 14:07:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,23 +8,22 @@ function makeId(name) {
     .replace(/^-|-$/g, "");
 }
 
-champions.forEach((champion) => {
+champions.forEach(function (champion) {
   const cardId = makeId(champion.name);
 
   const card = document.createElement("article");
   card.className = "card";
   card.id = cardId;
 
-  card.innerHTML = `
-    ${champion.image ? `<img class="champion-image" src="${champion.image}" alt="${champion.name}">` : ""}
-    <span class="badge">${champion.status}</span>
-    <h3>${champion.name}</h3>
-    <p><strong>${champion.category}</strong><br>${champion.address}</p>
-    <p>${champion.description}</p>
-    <ul>
-      ${champion.supports.map((support) => `<li>${support}</li>`).join("")}
-    </ul>
-  `;
+  card.innerHTML =
+    (champion.image ? '<img class="champion-image" src="' + champion.image + '" alt="' + champion.name + '">' : "") +
+    '<span class="badge">' + champion.status + '</span>' +
+    '<h3>' + champion.name + '</h3>' +
+    '<p><strong>' + champion.category + '</strong><br>' + champion.address + '</p>' +
+    '<p>' + champion.description + '</p>' +
+    '<ul>' + champion.supports.map(function (support) {
+      return '<li>' + support + '</li>';
+    }).join("") + '</ul>';
 
   cards.appendChild(card);
 });
@@ -36,14 +35,14 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "&copy; OpenStreetMap contributors"
 }).addTo(map);
 
-champions.forEach((champion) => {
+champions.forEach(function (champion) {
   const cardId = makeId(champion.name);
 
   L.marker([champion.lat, champion.lng])
     .addTo(map)
-    .bindPopup(`
-      <strong>${champion.name}</strong><br>
-      ${champion.category}<br>
-      <a href="#${cardId}">View champion details</a>
-    `);
+    .bindPopup(
+      "<strong>" + champion.name + "</strong><br>" +
+      champion.category + "<br>" +
+      '<a href="#' + cardId + '">View champion details</a>'
+    );
 });
