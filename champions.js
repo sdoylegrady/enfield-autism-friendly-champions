ccb9eca232a0776883459b87fdf9281f6f60f661
sdoylegrady <sdoylegrady@hotmail.com>
2026-06-19 13:20:34 +0100

    Update champions.js

--- champions.js
+++ champions.js
@@ -161,7 +161,7 @@ const champions = [
   description: "Local business supporting autism-friendly and inclusive access.",
   lat: 53.4143,
   lng: -6.8328,
-  image: "images/blossoms.png"
+  image: "images/champion blossoms.png"
 },
 
 {
