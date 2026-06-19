a12294ac8f04d50332402474da2d484174d6a70b
sdoylegrady <sdoylegrady@hotmail.com>
2026-06-19 12:39:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,29 +9,17 @@ champions.forEach((champion) => {
     <h3>${champion.name}</h3>
     <p><strong>${champion.category}</strong><br>${champion.address}</p>
     <p>${champion.description}</p>
-    <ul>${champion.supports.map((support) => `<li>${support}</li>`).join("")}</ul>
   `;
   cards.appendChild(card);
 });
 
-const validChampions = champions.filter((champion) =>
-  typeof champion.lat === "number" &&
-  typeof champion.lng === "number"
-);
-
 const map = L.map("map").setView([53.4142, -6.8326], 15);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution: "&copy; OpenStreetMap contributors"
 }).addTo(map);
 
-validChampions.forEach((champion) => {
-  L.marker([champion.lat, champion.lng])
-    .addTo(map)
-    .bindPopup(`<strong>${champion.name}</strong><br>${champion.status}`);
-});
-
-setTimeout(() => {
-  map.invalidateSize();
-}, 500);
+L.marker([53.4142, -6.8326])
+  .addTo(map)
+  .bindPopup("Enfield Autism-Friendly Champions");
