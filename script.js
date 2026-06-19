3b8043bca353af8c16350044905264d1c7684ecf
sdoylegrady <sdoylegrady@hotmail.com>
2026-06-19 14:21:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,19 @@
 const cards = document.getElementById("cards");
 
-function makeId(name) {
-  return name
-    .toLowerCase()
-    .replace(/&/g, "and")
-    .replace(/[^a-z0-9]+/g, "-")
-    .replace(/^-|-$/g, "");
-}
-
-champions.forEach(function (champion) {
-  const cardId = makeId(champion.name);
-
+champions.forEach((champion) => {
   const card = document.createElement("article");
   card.className = "card";
-  card.id = cardId;
 
-  card.innerHTML =
-    (champion.image ? '<img class="champion-image" src="' + champion.image + '" alt="' + champion.name + '">' : "") +
-    '<span class="badge">' + champion.status + '</span>' +
-    '<h3>' + champion.name + '</h3>' +
-    '<p><strong>' + champion.category + '</strong><br>' + champion.address + '</p>' +
-    '<p>' + champion.description + '</p>' +
-    '<ul>' + champion.supports.map(function (support) {
-      return '<li>' + support + '</li>';
-    }).join("") + '</ul>';
+  card.innerHTML = `
+    ${champion.image ? `<img class="champion-image" src="${champion.image}" alt="${champion.name}">` : ""}
+    <span class="badge">${champion.status}</span>
+    <h3>${champion.name}</h3>
+    <p><strong>${champion.category}</strong><br>${champion.address}</p>
+    <p>${champion.description}</p>
+    <ul>
+      ${champion.supports.map((support) => `<li>${support}</li>`).join("")}
+    </ul>
+  `;
 
   cards.appendChild(card);
 });
@@ -35,14 +25,16 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "&copy; OpenStreetMap contributors"
 }).addTo(map);
 
-champions.forEach(function (champion) {
-  const cardId = makeId(champion.name);
-
+champions.forEach((champion) => {
   L.marker([champion.lat, champion.lng])
     .addTo(map)
-    .bindPopup(
-      "<strong>" + champion.name + "</strong><br>" +
-      champion.category + "<br>" +
-      '<a href="#' + cardId + '">View champion details</a>'
-    );
+    .bindPopup(`
+      <strong>${champion.name}</strong><br>
+      ${champion.category}<br>
+      ${champion.status}
+    `);
 });
+
+setTimeout(() => {
+  map.invalidateSize();
+}, 500);
