8513148406245a05a0a22dbb1f7ff9d0b6423d59
sdoylegrady <sdoylegrady@hotmail.com>
2026-06-19 12:45:55 +0100

    Update champions.js

--- champions.js
+++ champions.js
@@ -5,9 +5,31 @@ const champions = [
     status: "Champion",
     address: "Main Street, Enfield, Co. Meath",
     supports: ["Welcoming space"],
-    description: "Test champion.",
+    description: "Local café supporting autism-friendly access.",
     lat: 53.4142,
     lng: -6.8326,
     image: "images/nomad.jpg"
+  },
+  {
+    name: "Lady M Boutique",
+    category: "Retail",
+    status: "Champion",
+    address: "Main Street, Enfield, Co. Meath",
+    supports: ["Welcoming environment", "Clear communication"],
+    description: "Local boutique supporting autism-friendly access.",
+    lat: 53.4141,
+    lng: -6.8329,
+    image: "images/lady-m.png"
+  },
+  {
+    name: "Murtagh Bros",
+    category: "Business",
+    status: "Champion",
+    address: "Main Street, Enfield, Co. Meath",
+    supports: ["Welcoming environment", "Clear communication"],
+    description: "Local business supporting the Enfield Champions initiative.",
+    lat: 53.4143,
+    lng: -6.8325,
+    image: "images/murtagh-bros.png"
   }
 ];
